7c784b3f8c61db44b7d1cbfce769ae79ba9eb10a
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-28 22:00:59 +0300

    feat: create class view

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import Game from "./js/game.js";
+import View from "./js/view.js";
+
+const root = document.querySelector('#root')
 
 const game = new Game();
+const view = new View(root, 480, 640, 20, 10);
 
 window.game = game;
+window.view = view;
