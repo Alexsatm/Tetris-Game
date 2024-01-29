b5325ca31d489b42bf431486ba3575caa3b12068
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-29 17:40:04 +0300

    add Listener

--- app.js
+++ app.js
@@ -9,4 +9,26 @@ const view = new View(root, 320, 640, 20, 10);
 window.game = game;
 window.view = view;
 
-view.renderPlayfiled(game.getState())
+document.addEventListener('keydown', e => {
+    switch (e.keyCode) {
+        case 37: //left arrow
+            game.movePieceLeft();
+            view.render(game.getState())
+            break;
+
+        case 38: //UP arrow
+            game.rotatePiece();
+            view.render(game.getState())
+            break;
+
+        case 39: //RIGHT arrow
+            game.movePieceRight();
+            view.render(game.getState())
+            break;
+
+        case 40: //DOWN arrow
+            game.movePieceDown();
+            view.render(game.getState())
+            break;
+    }
+})
