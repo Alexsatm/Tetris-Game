bb22dcf138203ab4807ed8e9a6379c6599de3ead
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-30 20:40:09 +0300

    add update

--- js/controller.js
+++ js/controller.js
@@ -2,31 +2,41 @@ export default class Controller {
     constructor(game, view) {
         this.game = game;
         this.view = view;
-        document.addEventListener('keydown', (e) => {
-            
-        })
-        }
+
+        setInterval(() => {
+            this.update();
+        }, 1000)
+
+        document.addEventListener('keydown', this.handleKeyDown.bind(this))
+        this.view.renderStartScreen();
+    }
+    update() {
+        this.game.movePieceDown();
+        this.view.renderMainScreen(this.game.getState());
+    }
 
         handleKeyDown(e) {
             switch (e.key) {
+                case 13: //Enter
+                    this.view.renderMainScreen(this.game.getState())
                 case 'ArrowLeft': //left arrow
-                    game.movePieceLeft();
-                    view.render(game.getState())
+                    this.game.movePieceLeft();
+                    this.view.renderMainScreen(game.getState())
                     break;
         
                 case 'ArrowUp': //UP arrow
-                    game.rotatePiece();
-                    view.render(game.getState())
+                    this.game.rotatePiece();
+                    this.view.renderMainScreen(game.getState())
                     break;
         
                 case 'ArrowRight': //RIGHT arrow
-                    game.movePieceRight();
-                    view.render(game.getState())
+                    this.game.movePieceRight();
+                    this.view.renderMainScreen(game.getState())
                     break;
         
                 case 'ArrowDown': //DOWN arrow
-                    game.movePieceDown();
-                    view.render(game.getState())
+                    this.game.movePieceDown();
+                    this.view.renderMainScreen(game.getState())
                     break;
             }
     }
